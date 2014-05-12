90d9cdf94dbdc20f95bc96305288e88a403da097
laudney <laudney@me.com>
2014-05-12 14:47:06 +0100

    Reddcoin PoW and PoSV miner. Later need to get rid of the duplicate in main.cpp

--- src/miner.h
+++ src/miner.h
@@ -3,28 +3,30 @@
 // Copyright (c) 2013 The NovaCoin developers
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
-#ifndef NOVACOIN_MINER_H
-#define NOVACOIN_MINER_H
+#ifndef REDDCOIN_MINER_H
+#define REDDCOIN_MINER_H
 
-#include "main.h"
-#include "wallet.h"
+#include <stdint.h>
+
+class CBlock;
+class CBlockIndex;
+struct CBlockTemplate;
+class CReserveKey;
+class CScript;
+class CWallet;
 
 /* Generate a new block, without valid proof-of-work */
-CBlock* CreateNewBlock(CWallet* pwallet, bool fProofOfStake=false, int64_t* pFees = 0);
-
+CBlockTemplate* CreateNewBlock(const CScript& scriptPubKeyIn);
+CBlockTemplate* CreateNewBlockWithKey(CReserveKey& reservekey);
 /** Modify the extranonce in a block */
 void IncrementExtraNonce(CBlock* pblock, CBlockIndex* pindexPrev, unsigned int& nExtraNonce);
-
 /** Do mining precalculation */
 void FormatHashBuffers(CBlock* pblock, char* pmidstate, char* pdata, char* phash1);
-
 /** Check mined proof-of-work block */
 bool CheckWork(CBlock* pblock, CWallet& wallet, CReserveKey& reservekey);
-
 /** Check mined proof-of-stake block */
-bool CheckStake(CBlock* pblock, CWallet& wallet);
-
+bool CheckStake(CBlock* pblock, CWallet& wallet, CReserveKey& reservekey);
 /** Base sha256 mining transform */
 void SHA256Transform(void* pstate, void* pinput, const void* pinit);
 
-#endif // NOVACOIN_MINER_H
+#endif // REDDCOIN_MINER_H
