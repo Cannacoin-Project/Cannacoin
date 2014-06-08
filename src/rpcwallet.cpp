2ce77bd922a3fbeb95cc12047dcd7bebbf243bdc
laudney <laudney@me.com>
2014-06-08 19:37:26 +0100

    In RPC call getinfo, also return unconfirmed balance.

--- src/rpcwallet.cpp
+++ src/rpcwallet.cpp
@@ -74,8 +74,9 @@ Value getinfo(const Array& params, bool fHelp)
     obj.push_back(Pair("version",       (int)CLIENT_VERSION));
     obj.push_back(Pair("protocolversion",(int)PROTOCOL_VERSION));
     if (pwalletMain) {
-        obj.push_back(Pair("walletversion", pwalletMain->GetVersion()));
-        obj.push_back(Pair("balance",       ValueFromAmount(pwalletMain->GetBalance())));
+        obj.push_back(Pair("walletversion",      pwalletMain->GetVersion()));
+        obj.push_back(Pair("balance",            ValueFromAmount(pwalletMain->GetBalance())));
+        obj.push_back(Pair("unconfirmedbalance", ValueFromAmount(pwalletMain->GetUnconfirmedBalance())));
     }
     obj.push_back(Pair("blocks",        (int)nBestHeight));
     obj.push_back(Pair("timeoffset",    (boost::int64_t)GetTimeOffset()));
