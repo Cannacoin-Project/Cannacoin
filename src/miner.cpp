bdf9b54ca5fa6d1bc522517153251353f41a6726
laudney <ren@reddcoin.com>
2014-09-18 19:39:29 +0100

    Sanitise tx fees.

--- src/miner.cpp
+++ src/miner.cpp
@@ -302,7 +302,7 @@ CBlockTemplate* CreateNewBlock(const CScript& scriptPubKeyIn)
             // Prioritize by fee once past the priority size or we run out of high-priority
             // transactions:
             if (!fSortedByFee &&
-                ((nBlockSize + nTxSize >= nBlockPrioritySize) || (dPriority < COIN * 576 / 250)))
+                ((nBlockSize + nTxSize >= nBlockPrioritySize) || (dPriority <= COIN * 1440 / 250)))
             {
                 fSortedByFee = true;
                 comparer = TxPriorityCompare(fSortedByFee);
