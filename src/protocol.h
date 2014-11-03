bd0f1e4bdebfe01c7f45ff6b73caf39c12af5334
root <root@vps29993.vps.ovh.ca>
2014-11-03 04:39:44 -0500

    stupid mistake, add port in protocol.h

--- src/protocol.h
+++ src/protocol.h
@@ -18,7 +18,7 @@
 extern bool fTestNet;
 static inline unsigned short GetDefaultPort(const bool testnet = fTestNet)
 {
-    return testnet ? 55444 : 45444;
+    return testnet ? 17143 : 7143;
 }
 
 
