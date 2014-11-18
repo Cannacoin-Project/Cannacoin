e01f9273c420f1cb76253b5e0a57daaea5f1ec89
Josh <Josh@cannacoin.cc>
2014-11-18 03:17:34 -0800

    Updated version to 2.0.0.1

--- src/version.h
+++ src/version.h
@@ -12,9 +12,9 @@
 //
 
 static const int CLIENT_VERSION =
-                           2000000 * CLIENT_VERSION_MAJOR
-                         +   00000 * CLIENT_VERSION_MINOR
-                         +     000 * CLIENT_VERSION_REVISION
+                           1000000 * CLIENT_VERSION_MAJOR
+                         +   10000 * CLIENT_VERSION_MINOR
+                         +     100 * CLIENT_VERSION_REVISION
                          +       1 * CLIENT_VERSION_BUILD; 
 
 extern const std::string CLIENT_NAME;
@@ -41,7 +41,7 @@ static const int PROTOCOL_VERSION = 2000001;
 static const int INIT_PROTO_VERSION = 209;
 
 // disconnect from peers older than this proto version
-static const int MIN_PEER_PROTO_VERSION = 1000000;
+static const int MIN_PEER_PROTO_VERSION = 2000000;
 
 // nTime field added to CAddress, starting with this version;
 // if possible, avoid requesting addresses nodes older than this
