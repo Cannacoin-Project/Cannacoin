a88223bed987bf1818041de285c935c78e8604e3
Josh <Josh@cannacoin.cc>
2014-11-23 00:57:16 -0800

    Adding easy linux startup

---
+++ doc/easy-linux-setup.sh
@@ -0,0 +1,7 @@
+sudo apt-get install build-essential
+sudo apt-get install libssl-dev
+sudo apt-get install libboost-all-dev
+sudo apt-get install libdb4.8++-dev
+sudo apt-get install libdb4.8
+sudo apt-get install libminiupnpc-dev 
+apt-get install qt4-qmake libqt4-dev build-essential libboost-dev libboost-system-dev libboost-filesystem-dev libboost-program-options-dev libboost-thread-dev libssl-dev libdb++-dev libminiupnpc-dev
