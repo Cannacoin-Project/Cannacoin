7485f8eabbad1f79dee5bab81d4339b0e0c48676
mikeyb <github@mikes.email>
2022-01-05 15:40:09 -0700

    add sudo to apt-get command

--- doc/easy-linux-setup.sh
+++ doc/easy-linux-setup.sh
@@ -4,4 +4,4 @@ sudo apt-get install libboost-all-dev
 sudo apt-get install libdb4.8++-dev
 sudo apt-get install libdb4.8
 sudo apt-get install libminiupnpc-dev 
-apt-get install qt4-qmake libqt4-dev build-essential libboost-dev libboost-system-dev libboost-filesystem-dev libboost-program-options-dev libboost-thread-dev libssl-dev libdb++-dev libminiupnpc-dev
+sudo apt-get install qt4-qmake libqt4-dev build-essential libboost-dev libboost-system-dev libboost-filesystem-dev libboost-program-options-dev libboost-thread-dev libssl-dev libdb++-dev libminiupnpc-dev
